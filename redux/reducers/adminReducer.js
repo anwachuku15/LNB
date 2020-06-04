478e699cf1cf0cf08faebeaaf2f9937de3aded09
anwachuku15 <anwachuku15@gmail.com>
2020-06-04 09:59:50 -0400

    added adminReducer

---
+++ redux/reducers/adminReducer.js
@@ -0,0 +1,64 @@
+import { CREATE_ANNOUNCEMENT, CREATE_ANNOUNCEMENT_NOIMG } from '../actions/adminActions'
+import { SET_ANNOUNCEMENTS, SET_ANNOUNCEMENT } from '../actions/authActions'
+import Announcement from '../../models/announcement-model'
+
+const initialState = {
+    announcements: [],
+    announcement: {}
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case SET_ANNOUNCEMENTS: {
+            return {
+                announcements: action.announcements
+            }
+        }
+        case SET_ANNOUNCEMENT: {
+            return {
+                ...state,
+                announcement: action.announcement,
+            }
+        }
+        case CREATE_ANNOUNCEMENT: 
+            const newAnnouncement = new Announcement(
+                action.postData.id,
+                action.postData.timestamp,
+                action.postData.uid,
+                action.postData.admin,
+                action.postData.adminHeadline,
+                action.postData.adminImage,
+                action.postData.body,
+                action.postData.imageUrl,
+                action.postData.likeCount,
+                action.postData.commentCount
+            )
+            return {
+                ...state,
+                announcements: state.announcements.concat(newAnnouncement),
+                read: action.read
+            }
+        
+        case CREATE_ANNOUNCEMENT_NOIMG: 
+            const newAnnouncementNoImg = new Announcement(
+                action.postData.id,
+                action.postData.timestamp,
+                action.postData.uid,
+                action.postData.admin,
+                action.postData.adminHeadline,
+                action.postData.adminImage,
+                action.postData.body,
+                action.postData.imageUrl,
+                action.postData.likeCount,
+                action.postData.commentCount
+            )
+            return {
+                ...state,
+                announcements: state.announcements.concat(newAnnouncementNoImg),
+                read: action.read
+            }
+        
+        default:
+            return state
+    }
+}
