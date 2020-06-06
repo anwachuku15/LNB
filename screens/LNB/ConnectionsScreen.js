9249e4b254018214fe1e1a232756d199b5937117
anwachuku15 <anwachuku15@gmail.com>
2020-06-06 05:48:02 -0400

    render user connections; will implement useReducer

--- screens/LNB/ConnectionsScreen.js
+++ screens/LNB/ConnectionsScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef, useEffect, useCallback, useReducer } from 'react'
 import { 
     Platform,
     View, 
@@ -6,118 +6,264 @@ import {
     StyleSheet, 
     Image, 
     Button, 
-    ScrollView 
+    ScrollView,
+    Keyboard,
+    TouchableWithoutFeedback,
+    TextInput,
+    FlatList
 } from 'react-native'
+import { withNavigationFocus } from 'react-navigation'
+import { ListItem } from 'react-native-elements'
 // REDUX
 import { useSelector, useDispatch } from 'react-redux'
 import Colors from '../../constants/Colors'
 import { useColorScheme } from 'react-native-appearance'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import HeaderButton from '../../components/UI/HeaderButton'
-import { FontAwesome } from '@expo/vector-icons'
+import { FontAwesome, Feather, MaterialIcons } from '@expo/vector-icons'
+import TouchableCmp from '../../components/LNB/TouchableCmp'
+
+
+import algoliasearch from 'algoliasearch'
+import { appId, key, adminkey } from '../../secrets/algolia'
+
+// const client = algoliasearch(appId, key)
+const client = algoliasearch(appId, adminkey)
+const connectionsIndex = client.initIndex('Connections')
+const index = client.initIndex('LNBmembers')
+
+// SET UP STATE (ON MOUNT) TO SOLVE CONNECTION SEARCH PROBLEM
 
 let themeColor
-let text
+let text, background
 const ConnectionsScreen = props => {
     const scheme = useColorScheme()
     let text
     if (scheme === 'dark') {
         themeColor = 'black'
         text = 'white'
+        background = 'black'
     } else {
         themeColor = 'white'
         text = 'black'
+        background = 'white'
     }
-
-    const dispatch = useDispatch()
     
-    const userId = props.navigation.getParam('userId')
-    const userName = props.navigation.getParam('userName')
     const auth = useSelector(state => state.auth)
     
+    const [search, setSearch] = useState('')
+    const [results, setResults] = useState([])
+    const [isFocused, setIsFocused] = useState(false)
+    const searchInput = useRef(null)
+    
+    let hits = []
+    let connectionHits = []
+    let connectionIds = []
+    let connections = []
+    let searchResults = []
+
+    const loadIndex = useCallback(async () => {
+        index.browseObjects({
+            query: '',
+            batch: batch => {
+                hits = hits.concat(batch)
+            }
+        })
+        connectionsIndex.browseObjects({
+            query: '',
+            batch: batch => {
+                connectionHits = connectionHits.concat(batch)
+            }
+        })
+        .then(() => {
+            const authId = auth.userId
+            connectionHits.forEach(hit => {
+                if (hit.objectID.includes(authId)) {
+                    connectionIds.push(hit.objectID.replace(authId,''))
+                }
+            })
+        })
+        .then(() => {
+            connections = connectionIds
+            hits.forEach(hit => {
+                if (connectionIds.includes(hit.objectID)) {
+                    searchResults.push(hit.newData)
+                }
+            })
+            setResults(searchResults)
+        })
+        .catch(err => console.log(err))
+    }, [])
+
+    useEffect(() => {
+        if (props.isFocused) {
+            loadIndex()
+        }
+    }, [loadIndex])
+
+
+    const updateSearch = (text) => {
+        setSearch(text)
+        const query = text
+
+        index.browseObjects({
+            query: query,
+            batch: batch => {
+                hits = hits.concat(batch)
+            }
+        })
+        connectionsIndex.browseObjects({
+            query: query,
+            batch: batch => {
+                connectionHits = connectionHits.concat(batch)
+            }
+        })
+        .then(() => {
+            const authId = auth.userId
+            connectionHits.forEach(hit => {
+                if (hit.objectID.includes(authId)) {
+                    connectionIds.push(hit.objectID.replace(authId,''))
+                }
+            })
+            return connectionIds
+        })
+        .then(ids => {
+            console.log(ids)
+            hits.forEach(hit => {
+                const newHit = hit.newData.uid
+                console.log(newHit)
+                if (ids.includes(hit.newData.uid)) {
+                    searchResults.push(hit.newData)
+                }
+            })
+            console.log('---')
+            setResults(searchResults)
+        }).catch(err => console.log(err))
+    }
+
+    const cancelSearch = () => {
+        searchInput.current.blur()
+    }
+
+    const DismissKeyboard = ({ children }) => (
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            {children}
+        </TouchableWithoutFeedback>
+    )
+
+    const navToUserProfile = (id, userName) => {
+        props.navigation.push('UserProfile', {
+            userId: id, 
+            name: userName
+        })
+        setSearch('')
+        
+    }
 
     const renderItem = ({item}) => (
-        // <Animated.View>
-            <TouchableCmp onPress={() => {
-                props.navigation.navigate({
-                    routeName: 'UserProfile',
-                    params: {
-                        userId: userId
-                    }
-                })
-            }}>
-                <ListItem
-                    containerStyle={{backgroundColor:background, paddingLeft: 0}}
-                    title={
-                        <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-                            <View style={{flexDirection:'row', width: '90%'}} >
-                                <View style={{width: '20%', alignItems:'center', justifyContent:'center'}}>
-                                    <Ionicons
-                                        name='md-person-add' 
-                                        size={23} 
-                                        color={Colors.blue}
-                                    />
-                                </View>
-                                <View style={{width: '80%'}}>
-                                    <TouchableCmp 
-                                        style={{alignSelf:'flex-start'}}
-                                        onPress={() => {navToUserProfile(item.senderId)}}
-                                    >
-                                        <Image 
-                                            source={{uri: item.senderImage}}
-                                            style={styles.avatar}
-                                        />
-                                    </TouchableCmp>
-                                    <Text style={{color:text, marginTop: 3}}>
-                                        You are now connected with <Text style={{fontWeight:'500', color:Colors.primary}}>{item.senderName}.</Text>
-                                    </Text>
-                                </View>
-                            </View>
-                        </View>
-                    }
-                    // subtitle='Content of what was liked or commented'
-                    // leftAvatar=
-                    bottomDivider
-                />
-            </TouchableCmp>
-        // </View>
+        <TouchableCmp onPress={() => {navToUserProfile(item.uid, item.name)}}>
+            <ListItem
+                containerStyle={{backgroundColor:background}}
+                leftAvatar={{source: {uri: item.imageUrl}}}
+                title={
+                    <Text style={{color:text, fontSize: 16}}>{item.name}</Text>
+                }
+                subtitle={
+                    <Text style={{color:Colors.disabled}}>
+                        {item.headline}{'\n'}<Text style={{fontSize:12}}>{item.location}</Text>
+                    </Text>
+                }
+                bottomDivider
+            />
+        </TouchableCmp>
     )
     return (
         
         <View style={styles.screen}>
-            <View style={styles.header}>
-                <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                    <Item
-                        title='Back'
-                        iconName={Platform.OS==='android' ? 'md-arrow-back' : 'ios-arrow-back'}
-                        onPress={() => {props.navigation.goBack()}}
+            <View style={{flexDirection:'row', paddingHorizontal: 10, marginTop: 10}}>
+                <View style={{...styles.searchContainer, width: isFocused ? '85%' : '100%', alignSelf: 'center'}}>
+                    <View style={{justifyContent:'center'}}>
+                        <Feather
+                            name='search'
+                            size={14}
+                            color={Colors.placeholder}
+                        />
+                    </View>
+                    <TextInput
+                        ref={searchInput}
+                        autoFocus={false}
+                        multiline={true}
+                        numberOfLines={4} 
+                        style={{flex:1, fontSize:14, color:text, marginLeft:7, marginRight:10, alignSelf:'center', paddingVertical:4}}
+                        placeholder={'Search...'}
+                        placeholderTextColor={Colors.placeholder}
+                        onChangeText={text => {updateSearch(text)}}
+                        value={search}
+                        onFocus={() => setIsFocused(true)}
+                        onBlur={() => setIsFocused(false)}
                     />
-                </HeaderButtons>
-                <Text style={styles.headerTitle}>{userName}</Text>
-                <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                    <Item
-                        title='Direct'
-                        iconName={Platform.OS==='android' ? 'md-more' : 'ios-more'}
-                        onPress={() => {}}
-                    />
-                </HeaderButtons>
+                    {search.length > 0 && (
+                        <TouchableCmp
+                            style={{justifyContent:'center'}}
+                            onPress={() => {
+                                setSearch('')
+                                updateSearch('')
+                            }}
+                        >
+                            <MaterialIcons
+                                name='cancel'
+                                size={16}
+                                color={Colors.disabled}
+                            />
+                        </TouchableCmp>
+                    )}
+                </View>
+                {isFocused ? (
+                    <TouchableCmp 
+                        onPress={() => {
+                            cancelSearch()
+                            updateSearch('')
+                        }}
+                        style={{width:'15%', alignItems:'center', alignSelf:'center', justifyContent:'center'}}
+                    >
+                        <Text style={{color: Colors.primary}}>Cancel</Text>
+                    </TouchableCmp>
+                ) : (
+                    null
+                )}
             </View>
-            <View style={{flex:1, justifyContent: 'center', alignItems:'center'}}>
-                <Text style={{color:Colors.socialdark}}>Under Construction</Text>
-                <FontAwesome name='gears' size={40} style={{marginTop: 10}} color={Colors.primary} />
-
-            </View>
+            {/* {!isFocused && (
+                <View style={{flex:1, justifyContent: 'center', alignItems:'center'}}>
+                    <Text style={{color:Colors.socialdark}}>Under Construction</Text>
+                    <Text style={{color:Colors.socialdark, fontSize: 12, marginTop:5}}>You can still search though :)</Text>
+                    <FontAwesome name='gears' size={40} style={{marginTop: 10}} color={Colors.primary} />
+                </View>
+            )} */}
+            {/* {isFocused && search.length === 0 && (
+                <DismissKeyboard>
+                    <View style={{flex: 1, alignItems:'center', paddingTop: 10}}>
+                        <Text style={{color:Colors.placeholder}}>Search for someone you'd like to connect with</Text>
+                    </View>
+                </DismissKeyboard>
+            )} */}
+            {/* {search.length > 0 && ( */}
+                <FlatList
+                    keyExtractor={(item, index) => index.toString()}
+                    data={results}
+                    renderItem={renderItem}
+                    showsVerticalScrollIndicator={false}
+                    showsHorizontalScrollIndicator={false}
+                />
+            {/* )} */}
         </View>
-
-            
     )
 }
 
 
 ConnectionsScreen.navigationOptions = (navData) => {
-    return {
-        headerTitle: 'Connections'
-    }
+    // return {
+    //     headerTitle: 'Connections'
+    // }
 }
 
 const styles = StyleSheet.create({
@@ -134,19 +280,19 @@ const styles = StyleSheet.create({
         borderBottomColor: Colors.primary,
         borderBottomWidth: StyleSheet.hairlineWidth
     },
-    header2: {
-        // borderBottomWidth: 0.5,
-        // shadowColor: Colors.primary,
-        // shadowOffset: {height: 5},
-        // shadowRadius: 15,
-        // shadowOpacity: 0.26,
-        // zIndex: 10
-    },
     headerTitle: {
         color: Colors.primary,
         fontFamily: 'open-sans-bold',
         fontSize: 17,
         fontWeight: '500'
     },
+    searchContainer: {
+        justifyContent: 'flex-end',
+        flexDirection: 'row',
+        paddingHorizontal: 5,
+        borderColor: Colors.primary,
+        borderWidth: 1,
+        borderRadius: 10,
+    },
 })
-export default ConnectionsScreen
+export default withNavigationFocus(ConnectionsScreen)
