1d95173181163a08218f85dc0eb27030fe647972
anwachuku15 <anwachuku15@gmail.com>
2020-06-16 20:05:20 -0400

    added pinned announcement component

--- screens/LNB/AnnouncementsScreen.js
+++ screens/LNB/AnnouncementsScreen.js
@@ -1,5 +1,7 @@
-import React, { useEffect, useCallback } from 'react'
+import React, { useEffect, useState, useCallback, useMemo } from 'react'
 import { 
+    Alert,
+    Platform,
     Dimensions,
     View, 
     Text, 
@@ -18,25 +20,27 @@ import { withNavigationFocus } from 'react-navigation'
 import Clipboard from '@react-native-community/clipboard'
 // REDUX
 import { useSelector, useDispatch } from 'react-redux'
-import { readAnnouncements } from '../../redux/actions/authActions'
+import { readAnnouncements, setAnnouncements } from '../../redux/actions/authActions'
+import { deleteAnnouncement, pinAnnouncement, unpinAnnouncement } from '../../redux/actions/adminActions'
 
 import Colors from '../../constants/Colors'
 import { useColorScheme } from 'react-native-appearance'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import HeaderButton from '../../components/UI/HeaderButton'
 import MessageIcon from '../../components/LNB/MessageIcon';
-import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { FontAwesome, Ionicons, MaterialCommunityIcons, AntDesign } from '@expo/vector-icons'
 import MenuAvatar from '../../components/LNB/MenuAvatar'
 import TouchableCmp from '../../components/LNB/TouchableCmp'
 import Lightbox from 'react-native-lightbox'
 import Hyperlink from 'react-native-hyperlink'
 import moment from 'moment'
+import PinnedAnnouncement from '../../components/LNB/PinnedAnnouncement'
 
 // import firebase from 'firebase'
 // const db = firebase.firestore()
 
-const WINDOW_WIDTH = Dimensions.get('window').width
-const WINDOW_HEIGHT = Dimensions.get('window').height
+const SCREEN_WIDTH = Dimensions.get('window').width
+const SCREEN_HEIGHT = Dimensions.get('window').height
 const BASE_PADDING = 10
 
 let themeColor
@@ -48,6 +52,10 @@ const AnnouncementsScreen = props => {
     const authUser = useSelector(state => state.auth.credentials)
     const uid = useSelector(state => state.auth.userId)
     let announcements = useSelector(state => state.admin.announcements.sort((a,b) => a.timestamp > b.timestamp ? -1 : 1))
+    let pinned = useSelector(state => state.admin.announcements.find(announcement => announcement.isPinned === true))
+    
+    const [error, setError] = useState()
+
     const dispatch = useDispatch()
     
     const scheme = useColorScheme()
@@ -61,12 +69,15 @@ const AnnouncementsScreen = props => {
         background = 'white'
     }
 
+    const [isVisible, setIsVisible] = useState(false)
+
     const loadAnnouncements = useCallback(async () => {
         try {
             await dispatch(readAnnouncements())
             console.log('blur')
         } catch (err) {
             console.log(err)
+            setError(err.message)
         }
     }, [dispatch]) 
 
@@ -78,7 +89,6 @@ const AnnouncementsScreen = props => {
     }, [loadAnnouncements])
 
     
-
     const selectUserHandler = (userId, name) => {
         props.navigation.navigate({
             routeName: 'UserProfile',
@@ -89,29 +99,208 @@ const AnnouncementsScreen = props => {
         })
     }
 
+    const [selectedItem, setSelectedItem] = useState()
 
+    const deleteHandler = (id) => {
+        console.log(id)
+        Alert.alert('Delete Announcement', 'Are you sure you want to delete this announcement?', [
+            {
+                text: 'Cancel',
+                style: 'cancel',
+                onPress: () => {
+                    setIsVisible(!isVisible)
+                    setSelectedItem()
+                }
+            },
+            {
+                text: 'Delete',
+                style: 'destructive',
+                onPress: async () => {
+                    try {
+                        await dispatch(deleteAnnouncement(id))
+                        setIsVisible(!isVisible)
+                        // setIsRefreshing(true)
+                        // loadData().then(() => {
+                        //     setIsRefreshing(false)
+                        // })
+                    } catch (err) {
+                        alert(err)
+                        console.log(err)
+                    }
+                    
+                }
+            }
+        ])
+    }
+
+    const pinHandler = (id) => {
+        Alert.alert('Pin Announcement', 'This will appear at the top of the announcement feed and replace any previously pinned announcement. Are you sure?', [
+            {
+                text: 'Cancel',
+                style: 'cancel',
+                onPress: () => {
+                    setIsVisible(!isVisible)
+                    setSelectedItem()
+                }
+            },
+            {
+                text: 'Pin',
+                style: 'default',
+                onPress: async () => {
+                    try {
+                        await dispatch(pinAnnouncement(id))
+                        setIsVisible(!isVisible)
+                        // setIsRefreshing(true)
+                        // loadData().then(() => {
+                        //     setIsRefreshing(false)
+                        // })
+                    } catch (err) {
+                        alert(err)
+                        console.log(err)
+                    }
+                    
+                }
+            }
+        ])
+    }
+
+    const unpinHandler = (id) => {
+        Alert.alert('Unpin Announcement', 'Are you sure?', [
+            {
+                text: 'Cancel',
+                style: 'cancel',
+                onPress: () => {
+                    setIsVisible(!isVisible)
+                    setSelectedItem()
+                }
+            },
+            {
+                text: 'Unpin',
+                style: 'destructive',
+                onPress: async () => {
+                    try {
+                        await dispatch(unpinAnnouncement(id))
+                        setIsVisible(!isVisible)
+                        // setIsRefreshing(true)
+                        // loadData().then(() => {
+                        //     setIsRefreshing(false)
+                        // })
+                    } catch (err) {
+                        alert(err)
+                        console.log(err)
+                    }
+                    
+                }
+            }
+        ])
+    }
+
+
+
+    const announcementOptions = (id) => {
+        setIsVisible(!isVisible)
+        setSelectedItem(id)
+        console.log(id)
+    }
 
     const renderItem = ({item}) => (
-        <TouchableCmp>
-            <View style={{...styles.announcementItem, backgroundColor: scheme==='dark' ? Colors.primaryDark : Colors.primaryLight}} key={item.id}>
-                <View style={{flexDirection:'row'}}>
-                    <TouchableCmp
-                        onPress={() => selectUserHandler(item.uid, item.admin)}
-                        style={{alignSelf:'flex-start'}}
-                    >
-                        <Image source={{uri: item.adminImage}} style={styles.avatar} />
-                    </TouchableCmp>
-                    
-                    <TouchableCmp onPress={() => selectUserHandler(item.uid, item.admin)}>
-                        <Text style={{...styles.name, ...{color:text}}}>
-                            {item.admin}
-                            <Text style={{...styles.timestamp, color: scheme==='dark' ? Colors.timestamp : Colors.placeholder}}>  ·  {moment(item.timestamp).fromNow()}</Text>
-                        </Text>
-                        <Text style={{...styles.headline, color: scheme==='dark' ? Colors.timestamp : Colors.disabled}}>{item.adminHeadline}</Text>
-                    </TouchableCmp>
-                </View>
+        item !== pinned &&
+        <TouchableCmp onPress={() => {}}>
+            <View style={{...styles.announcementItem, backgroundColor: scheme==='dark' ? 'black' : 'white'}} key={item.id}>
+                <TouchableCmp
+                    onPress={() => selectUserHandler(item.uid, item.admin)}
+                    style={{alignSelf:'flex-start'}}
+                >
+                    <Image source={{uri: item.adminImage}} style={styles.avatar} />
+                </TouchableCmp>
 
                 <View style={{flex: 1}}>
+                    <View style={{flexDirection: 'row', justifyContent:'space-between'}}>
+                        <TouchableCmp onPress={() => selectUserHandler(item.uid, item.admin)} style={{marginBottom: 5}}>
+                            <Text style={{...styles.name, ...{color:text}}}>
+                                {item.admin}
+                                <Text style={{...styles.timestamp, color: scheme==='dark' ? Colors.timestamp : Colors.placeholder}}>  ·  {moment(item.timestamp).fromNow()}</Text>
+                            </Text>
+                            <Text style={{...styles.headline, color: scheme==='dark' ? Colors.timestamp : Colors.disabled}}>{item.adminHeadline}</Text>
+                        </TouchableCmp>
+
+                        {authUser.isAdmin && (
+                            <View>
+                                <TouchableCmp onPress={() => announcementOptions(item.id)} style={{marginRight: 5}}>
+                                    <Ionicons
+                                        name={Platform.OS==='android' ? 'md-more' : 'ios-more'}
+                                        size={24}
+                                        color={Colors.disabled}
+                                    />
+                                </TouchableCmp>
+
+                                <Modal
+                                    animationType='slide'
+                                    transparent={true}
+                                    visible={isVisible}
+                                    onDismiss={() => {}}
+                                >
+                                    <View style={styles.modalView}>
+                                        <View style={{...styles.modal, backgroundColor: scheme==='dark' ? Colors.darkHeader : 'white'}}>
+                                            <TouchableCmp
+                                                style={{ ...styles.modalButton }}
+                                                onPress={() => deleteHandler(selectedItem)}
+                                            >
+                                                <View style={{flexDirection:'row', alignItems: 'center', marginLeft: 5}}>
+                                                    <Ionicons
+                                                        name={Platform.OS==='android' ? 'md-trash' : 'ios-trash'}
+                                                        color={Colors.redcrayola}
+                                                        size={28}
+                                                        style={{marginRight: 24}}
+                                                    />
+                                                    <Text style={{...styles.modalButtonText, color: Colors.redcrayola}}>Delete</Text>
+                                                </View>
+                                            </TouchableCmp>
+                                            <TouchableCmp
+                                                style={{ ...styles.modalButton, marginTop: 5, }}
+                                                onPress={() => {
+                                                    if (pinned) {
+                                                        selectedItem !== pinned.id ? pinHandler(selectedItem) : unpinHandler(selectedItem)
+                                                    } else pinHandler(selectedItem)
+                                                }}
+                                            >
+                                                <View style={{flexDirection:'row', alignItems: 'center'}}>
+                                                    <AntDesign
+                                                        name='pushpino'
+                                                        color={Colors.placeholder}
+                                                        size={24}
+                                                        style={{marginRight: 20}}
+                                                    />
+                                                    {!pinned || selectedItem !== pinned.id ? (
+                                                        <Text style={{...styles.modalButtonText, color: scheme==='dark' ? 'white' : 'black'}}>Pin Announcement</Text>
+                                                    ) : (
+                                                        <Text style={{...styles.modalButtonText, color: scheme==='dark' ? 'white' : 'black'}}>Unpin Announcement</Text>
+                                                    )}
+                                                </View>
+                                            </TouchableCmp>
+
+                                            <TouchableCmp
+                                                style={{marginTop: 5, backgroundColor: scheme==='dark' ? Colors.darkSearch : Colors.lightSearch, borderRadius: 20, padding: 12}}
+                                                onPress={() => {
+                                                    setIsVisible(!isVisible)
+                                                    setSelectedItem()
+                                                }}
+                                            >
+                                                <Text style={{...styles.modalButtonText, fontWeight:'bold', textAlign:'center', color: scheme==='dark' ? 'white' : 'black'}}>Cancel</Text>
+                                            </TouchableCmp>
+                                        </View>
+                                    </View>
+                                </Modal>
+                            </View>
+                        )}
+
+
+                    </View>
+
+                    {item.subject && (
+                        <Text style={styles.subject}>{item.subject}</Text>
+                    )}
+
                     <Hyperlink
                         linkDefault={true}
                         linkStyle={{color: scheme==='dark' ? Colors.bluesea : Colors.blue}}
@@ -154,6 +343,32 @@ const AnnouncementsScreen = props => {
         </TouchableCmp>
     )
 
+    if (error) {
+        return (
+            <View style={styles.spinner}>
+                <Text>An error occured</Text>
+                <Button title='try again' onPress={() => {}} color={Colors.primary}/>
+            </View>
+        )
+    }
+
+    const memoPinnedImages = useMemo(() => {
+        return (
+            <PinnedAnnouncement
+                pinned={pinned}
+                authUser={authUser}
+                selectUserHandler={selectUserHandler}
+                announcementOptions={announcementOptions}
+                deleteHandler={deleteHandler}
+                pinHandler={pinHandler} 
+                isVisible={isVisible}
+                setIsVisible={setIsVisible}
+                selectedItem={selectedItem}
+                setSelectedItem={setSelectedItem}
+            />
+        )
+    }, [selectUserHandler, announcementOptions, deleteHandler, pinHandler, isVisible, setIsVisible, selectedItem, setSelectedItem])
+
     return (
         <SafeAreaView style={styles.screen}>
 
@@ -162,11 +377,12 @@ const AnnouncementsScreen = props => {
                     keyExtractor={(item,index) => index.toString()}
                     data={announcements}
                     renderItem={renderItem}
+                    ListHeaderComponent={pinned && memoPinnedImages}
                 />
             ) : (
-                <View style={{flex:1, justifyContent: 'center', alignItems:'center'}}>
-                    <Text style={{color:Colors.socialdark}}>Under Construction</Text>
-                    <FontAwesome name='gears' size={40} style={{marginTop: 10}} color={Colors.primary} />
+                <View style={styles.spinner}>
+                    <Text>An error occured</Text>
+                    <Button title='try again' onPress={() => {}} color={Colors.primary}/>
                 </View>
             )}
         </SafeAreaView>
@@ -237,42 +453,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
-    modalView: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 22
-    },
-    modal: {
-        margin: 20,
-        backgroundColor: "white",
-        borderRadius: 20,
-        padding: 35,
-        alignItems: "center",
-        shadowColor: "#000",
-        shadowOffset: {
-          width: 0,
-          height: 2
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5
-    },
-    modalButton: {
-        backgroundColor: "#F194FF",
-        borderRadius: 20,
-        padding: 10,
-        elevation: 2
-    },
-    modalButtonText: {
-        color: "white",
-        fontWeight: "bold",
-        textAlign: "center"
-    },
-    modalText: {
-        marginBottom: 15,
-        textAlign: "center"
-    },
     screen: {
         flex: 1
     },
@@ -303,16 +483,19 @@ const styles = StyleSheet.create({
         // marginHorizontal: 16
     },
     announcementItem: {
-        // backgroundColor: '#FFF',
-        margin: 10,
+        flexDirection: 'row',
+        // margin: 10,
         padding: 8,
-        flexDirection: 'column',
-        borderRadius: 10
+        borderRadius: 10,
+        // borderWidth: 1,
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        borderColor: Colors.placeholder,
+        paddingBottom: 20,
     },
     avatar: {
-        width: 72,
-        height: 72,
-        borderRadius: 36,
+        width: 60,
+        height: 60,
+        borderRadius: 30,
         marginRight: 16
     },
     name: {
@@ -327,9 +510,16 @@ const styles = StyleSheet.create({
         fontSize: 16,
         marginTop: 4
     },
-    post: {
-        margin: 20,
+    subject: {
         fontSize: 18,
+        fontWeight: '500',
+        fontStyle: 'italic',
+        color: 'black',
+        marginTop: 10
+    },
+    post: {
+        marginTop: 5,
+        fontSize: 16,
         lineHeight: 22
     },
     postImage: {
@@ -342,8 +532,8 @@ const styles = StyleSheet.create({
         // marginRight: 20
     },
     lightboxImage: {
-        width: WINDOW_WIDTH,
-        height: WINDOW_HEIGHT - BASE_PADDING,
+        width: SCREEN_WIDTH,
+        height: SCREEN_HEIGHT - BASE_PADDING,
         borderRadius: 5,
         marginVertical: 10
     },
@@ -354,6 +544,43 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         margin: 10,
         alignSelf: 'flex-start',
+    },
+    modalView: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        marginTop: 22,
+        // backgroundColor: 'rgba(0,0,0,0.8)'
+    },
+    modal: {
+        width: Dimensions.get('window').width,
+        borderRadius: 20,
+        paddingTop: 30,
+        paddingBottom: 50,
+        paddingHorizontal: 20,
+        // alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+    },
+    modalButton: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+    },
+    modalButtonText: {
+        // fontWeight: "bold",
+        fontSize: 18,
+        // textAlign: "center",
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: "center",
     },
 })
 
