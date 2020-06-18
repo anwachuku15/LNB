423088046c347f69a933e554b9f918f13e5752c5
anwachuku15 <anwachuku15@gmail.com>
2020-06-17 22:11:05 -0400

    cleanup

--- screens/LNB/ConnectionsScreen.js
+++ screens/LNB/ConnectionsScreen.js
@@ -52,65 +52,29 @@ const ConnectionsScreen = props => {
         searchBar = Colors.lightSearch
     }
     
+    const dispatch = useDispatch()
+    
     const auth = useSelector(state => state.auth)
     const userId = props.navigation.getParam('userId')
     const userName = props.navigation.getParam('userName')
-
-    
-    const dispatch = useDispatch()
-    const [search, setSearch] = useState('')
-    const [results, setResults] = useState([])
-    // const [userConnections, setAllConnections] = useState([])
-    const [connections, setConnections] = useState([])
-    const [isMounted, setIsMounted] = useState(true)
-    const [isFocused, setIsFocused] = useState(false)
-    const [isLoading, setIsLoading] = useState(false)
-    const searchInput = useRef(null)
-    
-    let hits = []
-    let connectionHits = []
-    let searchResults = []
-
     const userConnections = useSelector(state => state.auth.userConnections)
 
-    const loadConnections = useCallback(async () => {
-        try {
-            await dispatch(fetchConnections(userId))
-        } catch (err) {
-            console.log(err)
-        }
-    }, [dispatch])
-
     
+    const [search, setSearch] = useState('')
+    const [results, setResults] = useState([])
 
-    useEffect(() => {
-        setIsMounted(true)
-        setIsLoading(true)
-        if (isMounted) {
-            loadConnections().then(() => {
-                setIsLoading(false)
-            })
-        }
-        
-        return () => {
-            setIsMounted(false)
-            loadConnections()
-        }
-    }, [dispatch, loadConnections, isMounted])
+    const searchInput = useRef(null)
+    
 
     const updateSearch = (text) => {
         setSearch(text)
-        if (text === '') {
-            setResults(userConnections)
-        } else {
-            const newResults = userConnections.filter(result => {
-                const resultData = `${result.name.toUpperCase()}`
-                const query = text.toUpperCase()
-    
-                return resultData.includes(query)
-            })
-            setResults(newResults)
-        }
+        const newResults = userConnections.filter(result => {
+            const resultData = `${result.name.toUpperCase()}`
+            const query = text.toUpperCase()
+
+            return resultData.includes(query)
+        })
+        setResults(newResults)
     }
 
     const cancelSearch = () => {
@@ -174,8 +138,7 @@ const ConnectionsScreen = props => {
                             onPress={async () => {
                                 await dispatch(getUser(item.uid))
                                 props.navigation.push(
-                                    'ChatScreen',
-                                    {
+                                    'ChatScreen', {
                                         selectedUserId: item.uid,
                                         userName: item.name,
                                         userImage: item.imageUrl
@@ -191,52 +154,10 @@ const ConnectionsScreen = props => {
                         </TouchableCmp>
                     </View>
                 }
-                // bottomDivider
             />
         </TouchableCmp>
     )
 
-    const renderSearchBar = () => (
-        <View style={{flexDirection:'row', paddingHorizontal: 10, marginTop: 10}}>
-            <View style={{...styles.searchContainer, backgroundColor: searchBar, width: '100%', alignSelf: 'center'}}>
-                <View style={{justifyContent:'center'}}>
-                    <Feather
-                        name='search'
-                        size={14}
-                        color={Colors.placeholder}
-                    />
-                </View>
-                <TextInput
-                    ref={searchInput}
-                    autoFocus={false}
-                    multiline={true}
-                    numberOfLines={4} 
-                    style={{flex:1, fontSize:16, color:text, marginLeft:7, marginRight:10, alignSelf:'center', paddingVertical:5}}
-                    placeholder={'Search...'}
-                    placeholderTextColor={Colors.placeholder}
-                    onChangeText={text => {updateSearch(text)}}
-                    value={search}
-                    // onFocus={() => setIsFocused(true)}
-                    onBlur={cancelSearch()}
-                />
-                {search.length > 0 && (
-                    <TouchableCmp
-                        style={{justifyContent:'center'}}
-                        onPress={() => {
-                            setSearch('')
-                            updateSearch('')
-                        }}
-                    >
-                        <MaterialIcons
-                            name='cancel'
-                            size={16}
-                            color={Colors.disabled}
-                        />
-                    </TouchableCmp>
-                )}
-            </View>
-        </View>
-    )
     return (
         <View style={styles.screen}>
             <View style={{flexDirection:'row', paddingHorizontal: 10, marginTop: 10}}>
@@ -258,15 +179,13 @@ const ConnectionsScreen = props => {
                         placeholderTextColor={Colors.placeholder}
                         onChangeText={text => {updateSearch(text)}}
                         value={search}
-                        onFocus={() => setIsFocused(true)}
-                        onBlur={() => setIsFocused(false)}
                     />
                     {search.length > 0 && (
                         <TouchableCmp
                             style={{justifyContent:'center'}}
                             onPress={() => {
                                 setSearch('')
-                                updateSearch('')
+                                // updateSearch('')
                             }}
                         >
                             <MaterialIcons
@@ -278,23 +197,15 @@ const ConnectionsScreen = props => {
                     )}
                 </View>
             </View>
-            {isLoading && (
-                <View style={styles.spinner}>
-                    <ActivityIndicator 
-                        size='large'
-                        color={Colors.primary}
-                    />
-                </View>
-            )}
             {userConnections && userConnections.length === 0 && (
                 <Text style={{color:Colors.placeholder, alignSelf: 'center', marginTop: 10}}>
                     {userId === auth.userId ? 'You have no connections' : `${userName} has no connections`}
                 </Text>
             )}
-            {!isLoading && userConnections && userConnections.length > 0 &&(
+            {userConnections && userConnections.length > 0 &&(
                 <FlatList
                     keyExtractor={(item, index) => index.toString()}
-                    data={isFocused ? results : userConnections}
+                    data={search.length > 0 ? results : userConnections}
                     renderItem={renderItem}
                     showsVerticalScrollIndicator={false}
                     showsHorizontalScrollIndicator={false}
