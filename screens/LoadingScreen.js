a120aa96984d924921f57139cdb19800a77b0048
anwachuku15 <anwachuku15@gmail.com>
2020-06-19 09:21:10 -0400

    added animated loading splash for future use

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,15 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { authenticate, getAuthenticatedUser } from '../redux/actions/authActions' 
-import { StyleSheet, View, ActivityIndicator, AsyncStorage } from 'react-native'
+import { StyleSheet, View, Text, Image, ActivityIndicator, AsyncStorage, MaskedViewIOS, Animated  } from 'react-native'
 import Colors from '../constants/Colors'
 import * as firebase from 'firebase'
 import jwtDecode from 'jwt-decode'
 import { saveDataToStorage } from '../redux/actions/authActions'
-// import { db } from '../Firebase/Firebase'
+import { useColorScheme } from 'react-native-appearance'
 
 const db = firebase.firestore()
+
+
+
 const LoadingScreen = props => {
+    // const scheme = useColorScheme()
+    // let background, text
+    // if (scheme === 'dark') {
+    //     background = 'black'
+    //     text = 'white'
+    // } 
+    // else {
+    //     background = 'white'
+    //     text = 'black'
+    // }
+
+    // const [loadingProgress, setLoadingProgress] = useState(new Animated.Value(0))
+    // const [animationDone, setAnimationDone] = useState(false)
+
     const dispatch = useDispatch()
     
     useEffect(() => {
@@ -49,12 +66,69 @@ const LoadingScreen = props => {
 
         tryLogin()
     }, [dispatch])
+    
+
+    
 
     return (
         <View style={styles.screen}>
             <ActivityIndicator size='large' color={Colors.primary} />
+            
         </View>
     )
+
+    // const colorLayer = animationDone ? null : <View style={[StyleSheet.absoluteFill, {backgroundColor: Colors.placeholder}]} />
+    // const themeColorLayer = <View style={[StyleSheet.absoluteFill, {backgroundColor: background}]} />
+
+
+    // useEffect(() => {
+    //     Animated.timing(loadingProgress, {
+    //         toValue: 100,
+    //         duration: 1000,
+    //         useNativeDriver: true,
+    //         delay: 800
+    //     }).start(() => {
+    //         setAnimationDone(true)
+    //     })
+    // },[])
+
+
+    // return (
+    //     <View style={styles.screen}>
+    //         {colorLayer}
+    //         <MaskedViewIOS 
+    //             style={{flex: 1}} 
+    //             maskElement={ 
+    //                 <View style={styles.screen}>
+    //                     <Animated.Image 
+    //                         source={require('../assets/lnb.png')}
+    //                         style={[{
+    //                             width: 1000, 
+    //                             transform: [{
+    //                                 scale: loadingProgress.interpolate({
+    //                                     inputRange: [0, 15, 100],
+    //                                     outputRange: [0.1, 0.06, 16]
+    //                                 })
+    //                             }]
+    //                         }]} 
+    //                         resizeMode='contain' 
+    //                     />
+    //                 </View>
+    //             }>
+    //             {themeColorLayer}
+    //             <Animated.View style={{
+    //                 ...styles.screen,
+    //                 opacity: loadingProgress.interpolate({
+    //                     inputRange: [0, 25, 50],
+    //                     outputRange: [0, 0, 1],
+    //                     extrapolate: 'clamp'
+    //                 })
+    //             }}>
+    //                 <Image source={require('../assets/lnb.png')} style={{width: 300}} resizeMode='contain'/>
+    //             </Animated.View>
+    //         </MaskedViewIOS>
+    //     </View>
+    // )
 }
 
 
@@ -62,7 +136,7 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
     }
 })
 
