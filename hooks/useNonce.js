aefde03804704666c6fffd05ec36e92975172b3c
anwachuku15 <anwachuku15@gmail.com>
2020-08-18 10:55:48 -0400

    added comment

--- hooks/useNonce.js
+++ hooks/useNonce.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { generateHexStringAsync } from 'expo-auth-session'
 
+// Generate a random hex string for the nonce parameter
 const useNonce = () => {
     const [nonce, setNonce] = useState(null);
 
