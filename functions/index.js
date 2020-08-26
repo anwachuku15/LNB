85a23dbd391aae55723a28eebb753942a3880acc
anwachuku15 <anwachuku15@gmail.com>
2020-08-26 19:08:43 -0400

    cleanup

--- functions/index.js
+++ functions/index.js
@@ -26,54 +26,54 @@ const client = algoliasearch(APP_ID, ADMIN_KEY)
 const index = client.initIndex('LNBmembers')
 const connectionsIndex = client.initIndex('Connections')
 
-exports.addToIndex = functions.firestore.document('/users/{userId}')
-  .onCreate(snapshot => {
-    const data = {
-      uid: snapshot.id,
-      name: snapshot.data().displayName,
-      headline: snapshot.data().headline,
-      location: snapshot.data().location,
-      bio: snapshot.data().bio,
-      imageUrl: snapshot.data().imageUrl,
-      website: snapshot.data().website,
-      connections: snapshot.data().connections
-    }
-    const objectID = snapshot.id
+// exports.addToIndex = functions.firestore.document('/users/{userId}')
+//   .onCreate(snapshot => {
+//     const data = {
+//       uid: snapshot.id,
+//       name: snapshot.data().displayName,
+//       headline: snapshot.data().headline,
+//       location: snapshot.data().location,
+//       bio: snapshot.data().bio,
+//       imageUrl: snapshot.data().imageUrl,
+//       website: snapshot.data().website,
+//       connections: snapshot.data().connections
+//     }
+//     const objectID = snapshot.id
 
-    return index.saveObject({ data, objectID })
-  })
+//     return index.saveObject({ data, objectID })
+//   })
 
-exports.updateIndex = functions.firestore.document('/users/{userId}')
-  .onUpdate((change) => {
-    const newData = {
-      uid: change.after.id,
-      name: change.after.data().displayName,
-      headline: change.after.data().headline,
-      location: change.after.data().location,
-      bio: change.after.data().bio,
-      imageUrl: change.after.data().imageUrl,
-      website: change.after.data().website,
-      connections: change.after.data().connections
-    }
-    const objectID = change.after.id
+// exports.updateIndex = functions.firestore.document('/users/{userId}')
+//   .onUpdate((change) => {
+//     const newData = {
+//       uid: change.after.id,
+//       name: change.after.data().displayName,
+//       headline: change.after.data().headline,
+//       location: change.after.data().location,
+//       bio: change.after.data().bio,
+//       imageUrl: change.after.data().imageUrl,
+//       website: change.after.data().website,
+//       connections: change.after.data().connections
+//     }
+//     const objectID = change.after.id
 
-    return index.saveObject({ newData, objectID })
-  })
+//     return index.saveObject({ newData, objectID })
+//   })
 
-exports.deleteFromIndex = functions.firestore.document('/users/{userId}')
-  .onDelete(snapshot => index.deleteObject(snapshot.id))
+// exports.deleteFromIndex = functions.firestore.document('/users/{userId}')
+//   .onDelete(snapshot => index.deleteObject(snapshot.id))
 
-exports.addToConnectionsIndex = functions.firestore.document('/connections/{connectionId}')
-  .onCreate(snapshot => {
-    const data = {
-      id: snapshot.id,
-      acceptedBy: snapshot.data().acceptedBy,
-      requestedBy: snapshot.data().requestedBy
-    }
-    const objectID = snapshot.id
+// exports.addToConnectionsIndex = functions.firestore.document('/connections/{connectionId}')
+//   .onCreate(snapshot => {
+//     const data = {
+//       id: snapshot.id,
+//       acceptedBy: snapshot.data().acceptedBy,
+//       requestedBy: snapshot.data().requestedBy
+//     }
+//     const objectID = snapshot.id
     
-    return connectionsIndex.saveObject({ data, objectID })
-  })
+//     return connectionsIndex.saveObject({ data, objectID })
+//   })
 
 
 
@@ -99,9 +99,12 @@ exports.addToConnectionsIndex = functions.firestore.document('/connections/{conn
 //   })
 
 
-exports.deleteFromConnectionsIndex = functions.firestore.document('/connections/{connectionId}')
-  .onDelete(snapshot => connectionsIndex.deleteObject(snapshot.id))
+// exports.deleteFromConnectionsIndex = functions.firestore.document('/connections/{connectionId}')
+//   .onDelete(snapshot => connectionsIndex.deleteObject(snapshot.id))
 
+exports.deleteUserFromFirestore = functions.auth.user().onDelete(user => {
+  db.doc(`/users/${user.uid}`).delete()
+})
 
 // UPDATE TWO COLLECTIONS: https://stackoverflow.com/questions/57653308/firestore-transaction-update-multiple-collections-in-a-single-transaction
 exports.onUserImageChange = functions
