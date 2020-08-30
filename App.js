3b8ed6eac79cbc852467ce5718bbe96175705997
anwachuku15 <anwachuku15@gmail.com>
2020-08-30 13:49:18 -0400

    added new fonts

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ import { StatusBar } from 'react-native'
 import { Provider } from 'react-redux'
 import store from './redux/store';
 import { useFonts } from '@use-expo/font'
+// import { NotoSansJP_500Medium } from '@expo-google-fonts/noto-sans-jp'
+import { Montserrat_500Medium_Italic } from '@expo-google-fonts/montserrat'
+import { MPLUSRounded1c_400Regular, MPLUSRounded1c_500Medium, MPLUSRounded1c_300Light } from '@expo-google-fonts/m-plus-rounded-1c'
 import { AppLoading } from 'expo'
 import { enableScreens } from 'react-native-screens'
 
@@ -22,19 +25,26 @@ export default function App() {
   
 
   const colorScheme = useColorScheme()
-  if(colorScheme === 'dark') {
-      StatusBar.setBarStyle('light-content')
+  let headerBackground
+  if (colorScheme === 'dark') {
+    StatusBar.setBarStyle('light-content')
+    headerBackground = '#1B1B1B'
   } else {
-      StatusBar.setBarStyle('dark-content')
+    StatusBar.setBarStyle('dark-content')
+    headerBackground = 'white'
   }
 
-  
   let [fontsLoaded] = useFonts({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
     'poppins': require('./assets/fonts/Poppins/Poppins-Regular.ttf'),
     'poppinsItalic': require('./assets/fonts/Poppins/Poppins-Italic.ttf'),
     'poppinsBold': require('./assets/fonts/Poppins/Poppins-Bold.ttf'),
+    'NotoSansJP_500Medium': require('./assets/fonts/Noto_Sans_JP/NotoSansJP-Medium.otf'),
+    Montserrat_500Medium_Italic,
+    MPLUSRounded1c_400Regular,
+    MPLUSRounded1c_500Medium,
+    MPLUSRounded1c_300Light,
   })
 
   const [isMounted, setIsMounted] = useState(true)
@@ -45,6 +55,8 @@ export default function App() {
       // console.log('App Unmounted')
     }
   })
+
+
 
   if(!fontsLoaded) {
     return <AppLoading />
