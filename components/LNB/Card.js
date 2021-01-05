2882cdd9ca45eaae5545259eea727ebd832dcf0e
anwachuku15 <anwachuku15@gmail.com>
2021-01-05 10:42:58 -0500

    prettier formatting

--- components/LNB/Card.js
+++ components/LNB/Card.js
@@ -1,34 +1,39 @@
-import React from 'react'
-import { View, StyleSheet } from 'react-native'
-import { useColorScheme } from 'react-native-appearance'
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { useColorScheme } from "react-native-appearance";
 
-
-const Card = props => {
-    // CARD THEME
-    const colorScheme = useColorScheme()
-    let cardTheme
-    if(colorScheme === 'dark') {
-        cardTheme = 'rgb(36,36,38)'
-    } else {
-        cardTheme = 'white'
-    }
-    return (
-        <View style={{...styles.card, ...{backgroundColor:cardTheme}, ...props.style}}>
-            {props.children}
-        </View>
-    )
-}
+const Card = (props) => {
+  // CARD THEME
+  const colorScheme = useColorScheme();
+  let cardTheme;
+  if (colorScheme === "dark") {
+    cardTheme = "rgb(36,36,38)";
+  } else {
+    cardTheme = "white";
+  }
+  return (
+    <View
+      style={{
+        ...styles.card,
+        ...{ backgroundColor: cardTheme },
+        ...props.style,
+      }}
+    >
+      {props.children}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    card: {
-        shadowColor: 'black',
-        shadowOpacity: 0.26,
-        shadowOffset: {width: 0, height: 2},
-        shadowRadius: 8,
-        elevation: 5,
-        borderRadius: 10,
-        // backgroundColor: 'white',
-    }
-})
+  card: {
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    // backgroundColor: 'white',
+  },
+});
 
-export default Card
+export default Card;
